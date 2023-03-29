042432ecb5e1d56398a92e9e574c097c2ea53f52
TanPhong <phongktbpham@gmail.com>
2023-03-29 22:34:53 +0700

    them 2 chuc nang cho phan header

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 // open and close the pushchair
-    var div = document.getElementById('puschair');
-    function openBtn(){
-        div.style.display = "block";
-        document.body.classList.add("fixed");
-    }
-    function closeBtn(){
-        div.style.display = "none";
-        document.body.classList.remove("fixed");
-    }
+var div = document.getElementById('puschair');
+function openBtn() {
+    div.style.display = "block";
+    document.body.classList.add("fixed");
+}
+function closeBtn() {
+    div.style.display = "none";
+    document.body.classList.remove("fixed");
+}
 // open and close the pushchair
+
+// scoll display
+
+const button = document.querySelector('.button');
+const element = document.querySelector('#present');
+
+// Bắt sự kiện click trên button
+button.addEventListener('click', () => {
+    // Cuộn màn hình tới phần tử
+    element.scrollIntoView({ behavior: 'smooth' });
+});
+// scoll display
