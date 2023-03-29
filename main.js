2fa2c08b094977dd4cb07ac78665d320e84877a9
TanPhong <phongktbpham@gmail.com>
2023-03-29 22:01:27 +0700

    Them .js và hoàn thiện ẩn hiện pushchair

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 // open and close the pushchair
-
+    var div = document.getElementById('puschair');
+    function openBtn(){
+        div.style.display = "block";
+        document.body.classList.add("fixed");
+    }
+    function closeBtn(){
+        div.style.display = "none";
+        document.body.classList.remove("fixed");
+    }
 // open and close the pushchair
